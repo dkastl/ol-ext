a71b7748ca3d6603c469c5d73f316cd109ab6d3e
Daniel Kastl <daniel@georepublic.de>
2024-06-06 19:54:43 +0900

    adds POST method

--- src/util/Ajax.js
+++ src/util/Ajax.js
@@ -1,4 +1,4 @@
-import ol_Object from 'ol/Object.js'
+import ol_Object from 'ol/Object.js';
 
 /** Ajax request
  * @fires success
@@ -31,8 +31,28 @@ var ol_ext_Ajax = class olextAjax extends ol_Object {
       ajax.on('success', function (e) { options.success(e.response, e); });
     if (options.error)
       ajax.on('error', function (e) { options.error(e); });
-    ajax.send(options.url, options.data, options.options);
+    ajax.send(options.url, options.data, 'GET', options.options);
   }
+  
+  /** Helper for post
+   * @param {*} options
+   *  @param {string} options.url
+   *  @param {string} options.auth Authorisation as btoa("username:password");
+   *  @param {string} options.dataType The type of data that you're expecting back from the server, default JSON
+   *  @param {string} options.success
+   *  @param {string} options.error
+   *  @param {*} options.data data to send in POST request
+   *  @param {*} options.options post options
+   */
+  static post(options) {
+    var ajax = new ol_ext_Ajax(options);
+    if (options.success)
+      ajax.on('success', function (e) { options.success(e.response, e); });
+    if (options.error)
+      ajax.on('error', function (e) { options.error(e); });
+    ajax.send(options.url, options.data, 'POST', options.options);
+  }
+  
   /** Helper to get cors header
    * @param {string} url
    * @param {string} callback
@@ -47,28 +67,38 @@ var ol_ext_Ajax = class olextAjax extends ol_Object {
       }
     };
   }
-  /** Send an ajax request (GET)
+  /** Send an ajax request (GET or POST)
    * @fires success
    * @fires error
    * @param {string} url
    * @param {*} data Data to send to the server as key / value
+   * @param {string} method HTTP method ('GET' or 'POST')
    * @param {*} options a set of options that are returned in the
    *  @param {boolean} options.abort false to prevent aborting the current request, default true
    */
-  send(url, data, options) {
+  send(url, data, method, options) {
     options = options || {};
     var self = this;
-    // Url
+    
+    // Encode URL if required
     var encode = (options.encode !== false);
     if (encode)
       url = encodeURI(url);
-
-    // Parameters
+    
+    // Prepare parameters
     var parameters = '';
-    for (var index in data) {
-      if (data.hasOwnProperty(index) && data[index] !== undefined) {
-        parameters += (parameters ? '&' : '?') + index + '=' + (encode ? encodeURIComponent(data[index]) : data[index]);
+    if (method === 'GET') {
+      // Encode parameters for GET
+      for (var index in data) {
+        if (data.hasOwnProperty(index) && data[index] !== undefined) {
+          parameters += (parameters ? '&' : '?') + index + '=' + encodeURIComponent(data[index]);
+        }
       }
+      url += parameters;
+      parameters = null; // For GET, parameters are in the URL, not in the body
+    } else if (method === 'POST') {
+      // Encode parameters for POST
+      parameters = new URLSearchParams(data).toString();
     }
 
     // Abort previous request
@@ -78,11 +108,14 @@ var ol_ext_Ajax = class olextAjax extends ol_Object {
 
     // New request
     var ajax = this._request = new XMLHttpRequest();
-    ajax.open('GET', url + parameters, true);
+    ajax.open(method, url, true);
     if (options.timeout)
       ajax.timeout = options.timeout;
     if (this._auth) {
       ajax.setRequestHeader("Authorization", "Basic " + this._auth);
+    }
+    if (method === 'POST') {
+      ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     }
 
     // Load complete
@@ -116,7 +149,6 @@ var ol_ext_Ajax = class olextAjax extends ol_Object {
           return;
         }
         // Success
-        //console.log('response',response)
         self.dispatchEvent({
           type: 'success',
           response: response,
@@ -161,8 +193,8 @@ var ol_ext_Ajax = class olextAjax extends ol_Object {
     };
 
     // GO!
-    ajax.send();
+    ajax.send(parameters);
   }
 }
 
-export default ol_ext_Ajax
+export default ol_ext_Ajax;
